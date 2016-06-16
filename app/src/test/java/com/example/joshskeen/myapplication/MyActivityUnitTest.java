64ea62d9b802f419e2e9f315e6cded18d1baa644
Michael Charland <mike_ch_1@hotmail.com>
2016-06-16 10:00:22 -0400

    jacoco coverage setup.Robolectric is not providing coverage numbers, so added unit test that does

--- app/src/test/java/com/example/joshskeen/myapplication/MyActivityUnitTest.java
+++ app/src/test/java/com/example/joshskeen/myapplication/MyActivityUnitTest.java
@@ -1,41 +1,18 @@
 package com.example.joshskeen.myapplication;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
-import org.robolectric.RobolectricGradleTestRunner;
-import org.robolectric.annotation.Config;
 
-import static org.assertj.android.api.Assertions.assertThat;
+import static junit.framework.TestCase.assertNotNull;
 
-
-@RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 21)
+/**
+ * Created by mcharland on 6/16/2016.
+ */
 public class MyActivityUnitTest
 {
-
-    private MyActivity mActivity;
-
-    @Before
-    public void setup()
-    {
-        mActivity = Robolectric.buildActivity(MyActivity.class).create().get();
-    }
-
     @Test
-    public void roboElectricMyActivityAppearsAsExpectedInitially()
+    public void test()
     {
-        assertThat(mActivity.mClickMeButton).hasText("Click me!");
-        assertThat(mActivity.mHelloWorldTextView).hasText("Hello world!");
+        MyActivity a = new MyActivity();
+        assertNotNull(a);
     }
-
-    @Test
-    public void roboElectricClickingClickMeButtonChangesHelloWorldText()
-    {
-        assertThat(mActivity.mHelloWorldTextView).hasText("Hello world!");
-        mActivity.mClickMeButton.performClick();
-        assertThat(mActivity.mHelloWorldTextView).hasText("HEY WORLD");
-    }
-
 }
---
+++ app/src/test/java/com/example/joshskeen/myapplication/RobolectricMyActivityUnitTest.java
@@ -0,0 +1,41 @@
+package com.example.joshskeen.myapplication;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.assertj.android.api.Assertions.assertThat;
+
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class RobolectricMyActivityUnitTest
+{
+
+    private MyActivity mActivity;
+
+    @Before
+    public void setup()
+    {
+        mActivity = Robolectric.buildActivity(MyActivity.class).create().get();
+    }
+
+    @Test
+    public void roboElectricMyActivityAppearsAsExpectedInitially()
+    {
+        assertThat(mActivity.mClickMeButton).hasText("Click me!");
+        assertThat(mActivity.mHelloWorldTextView).hasText("Hello world!");
+    }
+
+    @Test
+    public void roboElectricClickingClickMeButtonChangesHelloWorldText()
+    {
+        assertThat(mActivity.mHelloWorldTextView).hasText("Hello world!");
+        mActivity.mClickMeButton.performClick();
+        assertThat(mActivity.mHelloWorldTextView).hasText("HEY WORLD");
+    }
+
+}
